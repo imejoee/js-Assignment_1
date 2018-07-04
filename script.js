f695613ec41acda8c716cc4a47aa88fd40287260
imejoee <40078852+imejoee@users.noreply.github.com>
2018-07-03 22:51:32 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// this script is in head section.
+// when executed alerts "I am from <head>"
+alert("I am from <head>");
+
+// this script is in body section.
+// when executed alerts "I am from <body>"
+alert("I am from <body>");
+
+alert("I am from <body>");
